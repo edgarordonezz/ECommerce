d9327d612aa349f893889ea879ba73fa0e1478b1
edgarordonezz <1edgarordonez@gmail.com>
2026-03-17 11:57:03 -0500

    Add Role enum and integrate into User entity with EnumType.STRING

---
+++ src/main/java/com/example/ECommerce/Entity/Role.java
@@ -0,0 +1,6 @@
+package com.example.ECommerce.Entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/example/ECommerce/Entity/User.java
+++ src/main/java/com/example/ECommerce/Entity/User.java
@@ -6,10 +6,14 @@ A user has a id(PK), username, password(need to hash)
 package com.example.ECommerce.Entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -20,6 +24,7 @@ public class User {
     private Long id;
 
     @NotBlank
+    @Column(unique = true)
     private String username;
 
     // TODO: Hash password for security purposes
@@ -27,8 +32,11 @@ public class User {
     private String password;
 
     @NotBlank
+    @Email
+    @Column(unique = true)
     private String email;
 
     @NotBlank
+    @Enumerated(EnumType.STRING)
     private String role;
 }
